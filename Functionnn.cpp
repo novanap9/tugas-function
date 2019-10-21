195974a12a5672d0c4e8cebccaf307e6210300b6
novanap9 <56237209+novanap9@users.noreply.github.com>
2019-10-21 09:13:28 +0700

    Add files via upload

---
+++ Functionnn.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void Assalamualaikum() {
+		printf("									\n");
+		printf("Assalamualaikum wr.wb.\n");
+			printf("									\n");
+}
+
+
+
+void perkenalan() {
+	printf("Hai !!\n");
+	printf("											\n");
+	printf("Perkenalkan nama saya Novan Ari Pradana\n");
+	printf("Asal Saya Dari Tuban\n");
+	printf("Saya sekarang tinggal di Surabaya\n");
+	printf("Saya berkuliah di Universitas Negeri Surabaya\n");
+	printf("Motto Saya : Aku dan Kamu menjadi KITA\n");
+	printf("Harapan kedepannya saya ingin menjadi pengusaha sukses di bidang IT\n");
+	printf("   \n");
+	printf("\n");
+}
+
+void penutupan() {
+	printf(" \n");
+	printf("Sekian\n");
+	printf("Wassalamualaikum wr.wb.\n");
+	printf("									\n");
+}
+
+int main () {
+	Assalamualaikum();
+  	perkenalan();
+  	penutupan();
+}
